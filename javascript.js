97340325452f071a329968d7d70f690a0603c02f
creinken <reinkendigital@gmail.com>
2025-05-30 05:22:29 -0500

    Add operate function
    Added operate function which takes 2 numbers and an operand and computes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
+let a, b = 0;
+let operand = '';
+
+function operate(a, b, operand) {
+    let result
+    switch (operand) {
+        case '+':
+            result = add(a, b);
+            break;
+        
+        case '-':
+            result = subtract(a, b);
+            break;
+        
+        case '*':
+            result = multiply(a, b);
+            break;
+
+        case '/':
+            result = divide(a, b)
+            break;
+
+        default:
+            break;
+    }
+    return result;
+}
+
 function add(a, b) {
     return a + b;
 }
